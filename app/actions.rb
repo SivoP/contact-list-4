2939dea3c58a065147cffb30da38f0462e50222c
SivoP <siv.padhy@gmail.com>
2015-07-29 00:13:38 +0000

    ajax functionality added to index page, form updates database through ajax vs default method

--- app/actions.rb
+++ app/actions.rb
@@ -3,15 +3,46 @@
 
 get '/' do
   @contacts = Contact.all
+  @contacs.to_json
   erb :index
 end
 
+post '/' do 
+
+end
+
+
 get '/new' do
-  
+  @contact = Contact.find(2)
+  @contact.to_json
+end
+
+get '/contacts' do 
+  @contacts = Contact.all
+  @contacs.to_json
+
+end
+
+
+
+post '/contacts' do
+
+  @contact = Contact.new(
+    firstname: params[:firstname],
+    lastname: params[:lastname],
+    phone: params[:phone],
+    email: params[:email],
+    cell: params[:cell]
+    )
+  @contact.save!
+  @contact.to_json
 end
 
 get '/edit/:id' do
+  @contact = Contact.find(params[:id])
+  @contact.to_json
 end
+
 
 
 
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,50 @@
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+ 
+  var form = $("#new-form")
+  $("#new-form").on('submit', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    var firstname = $("#form-firstname").val();
+    var lastname = $("#form-lastname").val();  
+    var phone = $("#form-phone").val();
+    var email = $("#form-email").val();
+
+    var formData = {
+      'firstname': firstname,
+      'lastname' : lastname,
+      'phone'    : phone,
+      'email'    : email
+    }
+    
+      $.ajax({
+        type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+        url         : '/contacts', // the url where we want to POST
+        data        : formData, // our data object
+        dataType    : 'json', // what type of data do we expect back from the server
+                    encode          : true
+    });
+
+
+      
+
+
+
+    });
+
+
+  
+
+
+
+
+
+
+ 
+
+
+
+
+
+
 });
